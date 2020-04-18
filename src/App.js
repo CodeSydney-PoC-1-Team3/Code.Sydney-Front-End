1648d090353ef2b0b283cd518eb17acdf55996c3
Prashanga <prashanga@gmail.com>
2020-04-18 11:54:25 +1000

    DEV-001: Moving mentor's state up to the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Mentorlist from './components/Mentorlist'
 import './App.css'
 
 function App() {
+
+  const [ mentors, setMentors ] = useState([])
+  const [ loading, setLoading ] = useState(true)
+
+  useEffect(() => {
+      const getMentors = async () => {
+          try{
+              const response = await axios.get('https://codesydney-mentors.herokuapp.com/api/v1/mentors')
+              console.log(response.data.data.mentors)
+              setMentors(response.data.data.mentors)
+              setLoading(false)
+          }
+          catch(e){
+              console.log(e.message)
+          }
+      }
+      getMentors()
+      
+  },[])
+
+
   return (
-    <div className="App">
-      <Mentorlist />
-    </div>
+    <>
+     {loading === true && 'Loading......'}
+      <div className="App">
+        <Mentorlist mentors={mentors}/>
+      </div>
+    </>
   );
 }
 
--- src/components/Mentorlist.js
+++ src/components/Mentorlist.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import placeholder from '../img/placeholder.png'
 
-const Mentorlist = () => {
-
-    const [ mentors, setMentors ] = useState([])
-    const [ loading, setLoading ] = useState(true)
-
-    useEffect(() => {
-        const getMentors = async () => {
-            try{
-                const response = await axios.get('https://codesydney-mentors.herokuapp.com/api/v1/mentors')
-                console.log(response.data.data.mentors)
-                setMentors(response.data.data.mentors)
-                setLoading(false)
-            }
-            catch(e){
-                console.log(e.message)
-            }
-        }
-        getMentors()
-        
-    },[])
+const Mentorlist = ({ mentors }) => {
 
     const getMentorPic = (mentor) => {
 
         return mentor.photo === true ?
             `https://codesydney-website.s3-ap-southeast-2.amazonaws.com/mentor/${mentor.id}.png`
             : placeholder
-
-        
-     
     }
 
     return(
         <>
-            {loading === true && <>.....Loading</>}
             {
                 mentors.map( mentor => {
                     return(
